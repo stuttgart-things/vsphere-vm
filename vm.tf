21221ec7c22cf569e4fc5338aeb08268408e9e10
patrick hermann <47567770+patrick-hermann-sva@users.noreply.github.com>
2024-01-03 09:20:27 +0100

    Update vm.tf

--- vm.tf
+++ vm.tf
@@ -19,8 +19,8 @@ resource "vsphere_virtual_machine" "vm" {
   disk {
     label            = var.vm_disk_label
     size             = var.vm_disk_size
-    eagerly_scrub    = data.vsphere_virtual_machine.template.disks.0.eagerly_scrub
-    thin_provisioned = data.vsphere_virtual_machine.template.disks.0.thin_provisioned
+    eagerly_scrub    = data.vsphere_virtual_machine.template.disks[0].eagerly_scrub
+    thin_provisioned = data.vsphere_virtual_machine.template.disks[0].thin_provisioned
   }
   clone {
     template_uuid = data.vsphere_virtual_machine.template.id
