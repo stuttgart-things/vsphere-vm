96f546a4ad358a6b1572ac428f78547d244e7661
patrick hermann <47567770+patrick-hermann-sva@users.noreply.github.com>
2024-01-22 18:39:35 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,3 +8,10 @@ terraform {
     }
   }
 }
+
+provider "vsphere" {
+  user                 = var.vsphere_user
+  password             = var.vsphere_password
+  vsphere_server       = var.vsphere_server
+  allow_unverified_ssl = var.unverified_ssl
+}
